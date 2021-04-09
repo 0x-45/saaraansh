aef6167d036a12dd661956cf34c03679803047e2
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-04-09 15:55:49 +0530

    Nav changed

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,7 +5,13 @@ export const Nav = () => {
   return (
     <div className="md:flex-row flex flex-col items-center justify-between p-3 px-10">
       <div className="text-5xl font-bold text-red-600 uppercase">
-        <Link href="/">Saaransh</Link>
+        <Link href="/">
+          <img
+            src="thumbnail.png"
+            alt="Saaransh Logo and moto"
+            className="h-32 cursor-pointer"
+          />
+        </Link>
       </div>
       <div className="flex flex-row">
         <Link href="/">
