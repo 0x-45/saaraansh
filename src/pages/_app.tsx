1e1053a05276b7082b126db8e4dca0339721e295
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-04-11 02:36:17 +0530

    next-pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,23 +10,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       duration: 2000,
     });
   }, []);
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", function () {
-        navigator.serviceWorker.register("/pwabuilder-sw.js").then(
-          function (registration) {
-            console.log(
-              "Service Worker registration successful with scope: ",
-              registration.scope
-            );
-          },
-          function (err) {
-            console.log("Service Worker registration failed: ", err);
-          }
-        );
-      });
-    }
-  }, []);
 
   return <Component {...pageProps} />;
 }
