de74f48c580da4de1d9644372138322f5ebd0296
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-04-11 03:09:26 +0530

    manifest -> _doc.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,6 +20,7 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
             rel="stylesheet"></link>
+          <link rel="manifest" href="manifest.json" />
         </Head>
         <body className="font-body" style={{ backgroundColor: "#DAE0E6" }}>
           <Main />
